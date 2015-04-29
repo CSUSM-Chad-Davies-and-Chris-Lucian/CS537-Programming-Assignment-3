0e44f338b400e448d51d83f59f3c345dd9359cb0
Chris Lucian <Chris@DeadlyApps.com>
2015-04-29 08:18:05 -0700

    Test mode parameter passed to individual runs

--- tests.cpp
+++ tests.cpp
@@ -11,33 +11,31 @@
 
 using namespace std;
 
+void run_tests_with_test_mode(string port1, string test_mode)
+{
+  // kill all clients and servers before executing a test
+  system("echo \e[92mKilling Old Processes:\e[0m");
+  system("ps | grep server | awk '{print $1}' | xargs kill");
+  system("ps | grep client | awk '{print $1}' | xargs kill");
+
+  // construct the server command and start the test
+  printf("\nTESTS: Starting Server App\n");
+  string command = "./server " + port1 + " " + test_mode + " &";
+  system(command.c_str());
+
+  // Create the client to send the server data.
+  printf("\nTESTS: Starting Client App %d\n",index);
+  string command2 = "./client localhost " + port1 + " " + test_mode + " &";
+  system(command2.c_str());
+}
+
 // this program automates the testing of the two applications with a single
 // argument of a port
 int main(int argc, char *argv[]) {
     string port1;
     port1 = argv[1];
 
-    // kill all clients and servers before executing a test
-    system("echo \e[92mKilling Old Processes:\e[0m");
-    system("ps | grep server | awk '{print $1}' | xargs kill");
-    system("ps | grep client | awk '{print $1}' | xargs kill");
-
-    // construct the server command and start the test
-    printf("\nTESTS: Starting Server App\n");
-    string command = "./server " + port1 + " &";
-    system(command.c_str());
-
-    // Create the client to send the server data.
-    printf("\nTESTS: Starting Client App %d\n",index);
-    string command2 = "./client localhost " + port1 + " &";
-    system(command2.c_str());
-
-
-
-
-
-
-
+    run_tests_with_test_mode(port1, "None");
 
     return 0;
 }
